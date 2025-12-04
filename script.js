e53929d3dc7100f507489180bc2875e2ee49f40c
Reshmasudeepa <reshmasudeepa1@gmail.com>
2025-12-04 21:30:43 +0530

    updated script.js

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 console.log("Hello World!");
-let x = "raju";
-console.log(x)
+//let x = "raju";
+//console.log(x)
 
 const pie = 3.14
 const PIE = 4.55
 console.log(pie)
 console.log(PIE)
+
+let stringvalue = "10";
+let integervalue = 10;
+let floatvalue = 10.100;
+let booleanvalue = true;
+console.log(typeof stringvalue);
+console.log(typeof integervalue);
+console.log(typeof floatvalue);
+console.log(typeof booleanvalue);
+
+let x = 10;
+let y = 10;
+
+let addition = x + y;
+let sub = x - y;
+let multi = x * y;
+let division = x / y;
+let remainder = x % y;
+
+console.log(addition)
+console.log(sub)
+console.log(multi)
+console.log(division)
+console.log(remainder)
